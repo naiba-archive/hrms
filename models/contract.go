325c367398f0c0b3db8cb0c62bbaef2a08c11115
奶爸 <hi@nai.ba>
2019-08-01 10:24:54 +0800

    :building_construction: Update Models

---
+++ models/contract.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	_ = iota
+	// ContractTypeToLandlord 跟房东签的合同
+	ContractTypeToLandlord
+	// ContractTypeToCompany 跟租户签的合同
+	ContractTypeToCompany
+)
+
+// Contract 合同
+type Contract struct {
+	gorm.Model
+	HouseID         uint      // 绑定房间
+	PartyA          uint      // 租客 · 甲方
+	PartyB          uint      // 业务员 · 乙方
+	Type            uint      // 合同类型
+	Deadline        time.Time // 截止时间
+	FirstPaymentAt  time.Time // 第一次缴费时间
+	PaymentDuration time.Time // 缴费间隔
+	Establishment   time.Time // 建立时间
+}
--- models/house.go
+++ models/house.go
@@ -7,19 +7,19 @@ import (
 // House 房屋信息表
 type House struct {
 	gorm.Model
-	ParentID        uint    // 母房屋（拆间合租）
-	CompanyID       uint    // 所属公司
-	SKU             string  // 房间属性 例如 kt,xy,yb 代表 空调 向阳 浴霸
-	Landlord        string  // 房东
-	LandlordContact string  // 房东联系方式
-	Pics            string  // 房间图数组
-	Note            string  // 房间备注
-	Province        string  // 省
-	Prefecture      string  // 市
-	County          string  // 县（区）
-	Street          string  // 街道
-	Community       string  // 小区
-	Address         string  // 详细地址
-	GisX            float64 // 经度
-	GixY            float64 // 纬度
+	LandlordID uint    // 房东
+	ManagerID  uint    // 房屋管理员
+	CompanyID  uint    // 所属公司
+	ParentID   uint    // 母房屋（拆间合租）
+	SKU        string  // 房间属性 例如 kt,xy,yb 代表 空调 向阳 浴霸
+	Pics       string  // 房间图数组
+	Note       string  // 房间备注
+	Province   string  // 省
+	Prefecture string  // 市
+	County     string  // 县（区）
+	Street     string  // 街道
+	Community  string  // 小区
+	Address    string  // 详细地址
+	GisX       float64 // 经度
+	GixY       float64 // 纬度
 }
